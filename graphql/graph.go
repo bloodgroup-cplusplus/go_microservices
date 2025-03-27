d840d46878dcdf529402f7c7647411c48aa33517
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-03-27 12:48:25 +0530

    adding graph for graphql microservices

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,1 +1,31 @@
 package main
+
+// central file to the whole api gateway
+// with the help of server we can call the account product and order client
+
+type Server struct {
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	orderClient   *order.Client
+}
+
+func NewGraphQLserver(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+
+	accountClient, err := account.NewClient(accountUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	catalogClient, err := catalog.NewClient(catalogUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+	orderClient, err := order.NewClient(orderUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+}
