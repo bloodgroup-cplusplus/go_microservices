2748322cc9a693f0a716f33eeb9a86b67116bd2f
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-03-28 09:04:53 +0530

    adding envconfig and handler

--- graphql/main.go
+++ graphql/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/handler"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type AppConfig struct {
+	AccountURL   string `envconfig:"ACCOUNT_SERVICE_URL"`
+	CatalogueURL string `envconfig:"CATALOGUE_SERVICE_URL"`
+	OrderURL     string `envconfig:"ORDER_SERVICE_URL"`
+}
+
+func main() {
+	var cfg AppConfig
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, err := NewGraphQLserver(cfg.AccountURL, cfg.CatalogueURL, cfg.OrderURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle("/graphql", handler.GraphQL(s.ToExecutableSchema()))
+
+}
